783e09f707ca87bc93f5da17efbb5284fe98f7c3
Vivind15 <vivinddelekar08@gmail.com>
2026-02-12 03:06:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.getElementById("search").addEventListener("keyup", function() {
+    let value = this.value.toLowerCase();
+    let properties = document.querySelectorAll(".property");
+
+    properties.forEach(function(property) {
+        let city = property.getAttribute("data-city").toLowerCase();
+        if (city.includes(value)) {
+            property.style.display = "inline-block";
+        } else {
+            property.style.display = "none";
+        }
+    });
+});
 
